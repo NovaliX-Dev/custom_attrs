9b39dc1e79b9f18f173f88b327a4768114829b25
NovaliX <novalix@protonmail.com>
2023-02-17 23:56:21 +0100

    added option check to attribute_not_set test

--- tests/compile_fail/attribute_not_set.rs
+++ tests/compile_fail/attribute_not_set.rs
@@ -9,6 +9,15 @@ enum Enum {
     Variant2
 }
 
+#[derive(CustomAttrs)]
+#[attr(pub a: Option<usize>)]
+enum Enum2 {
+    Variant1,
+
+    #[attr(a = 3)]
+    Variant2
+}
+
 fn main() {
     // let _a = Enum::Variant1.get_a();
 }
